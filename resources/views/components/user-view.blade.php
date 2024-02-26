e695a6e7b744b4641c7e7c2090de2dad192f16f0
rohit0814 <singhrohit.rs747@gmail.com>
2024-02-26 21:08:14 +0530

    dashboard ui created

---
+++ resources/views/components/user-view.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blogger</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <div class="container">
+        <header class="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/"
+                class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+                <svg class="bi me-2" width="40" height="32">
+                    <use xlink:href="#bootstrap"></use>
+                </svg>
+                <span class="fs-4">{{ Auth::guard('blogger')->user()->first_name }}
+                    {{ Auth::guard('blogger')->user()->last_name }}</span>
+            </a>
+
+            <ul class="nav nav-pills">
+                <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Dashboard</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Post</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Trash</a></li>
+                <li class="nav-item"><a href="#" class="nav-link">Profile</a></li>
+                <form action="/users/logout" method="post">
+                    @csrf
+                    <li class="nav-item"><button type="submit" class="nav-link">Logout</button></li>
+                </form>
+            </ul>
+        </header>
+    </div>
+
+
+    <div class="container">
+        {{ $slot }}
+    </div>
+
+
+
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <div class="col-md-4 d-flex align-items-center">
+                <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+                    <svg class="bi" width="30" height="24">
+                        <use xlink:href="#bootstrap"></use>
+                    </svg>
+                </a>
+                <span class="mb-3 mb-md-0 text-body-secondary">© 2024 Company, Inc</span>
+            </div>
+
+            <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+                <li class="ms-3"><a class="text-body-secondary" href="#"><i
+                            class="fa-brands fa-facebook"></i></a></li>
+                <li class="ms-3"><a class="text-body-secondary" href="#"><i
+                            class="fa-brands fa-square-instagram"></i></a></li>
+                <li class="ms-3"><a class="text-body-secondary" href="#"><i
+                            class="fa-brands fa-x-twitter"></i></a></li>
+            </ul>
+        </footer>
+    </div>
+</body>
+
+</html>
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,13 +9,95 @@
 </head>
 
 <body>
-    <h1>Welcome {{ Auth::guard('blogger')->user()->first_name }} {{ Auth::guard('blogger')->user()->last_name }}</h1>
+
+    <x-user-view>
+        <div class="my-3 p-3 bg-body rounded shadow-sm">
+            <h6 class="border-bottom pb-2 mb-0">Posts</h6>
+            <div class="d-flex text-body-secondary pt-3">
+                <i class="fa-solid fa-blog" style="position: relative; top:7px; left:26px; color:white"></i>
+                <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                    xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                    preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
+                        dy=".3em">32x32</text>
+                </svg>
+                <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <strong class="text-gray-dark">PostTitle</strong>
+                            <span class="d-block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+                                in............</span>
+                        </div>
+
+                        <div>
+                            <a href="#" type="button" class="btn btn-warning">Edit <i
+                                    class="fa-solid fa-pen-to-square"></i></a> &nbsp; &nbsp;
+                            <a href="#" type="button" class="btn btn-danger">Move to Trash <i
+                                    class="fa-brands fa-bitbucket"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
 
-    <form action="/users/logout" method="post">
-        @csrf
-        <button type="submit">Logout</button>
-    </form>
+            <div class="d-flex text-body-secondary pt-3">
+                <i class="fa-solid fa-blog" style="position: relative; top:7px; left:26px; color:white"></i>
+                <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                    xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                    preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
+                        dy=".3em">32x32</text>
+                </svg>
+                <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <strong class="text-gray-dark">PostTitle</strong>
+                            <span class="d-block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+                                in............</span>
+                        </div>
+
+                        <div>
+                            <a href="#" type="button" class="btn btn-warning">Edit <i
+                                    class="fa-solid fa-pen-to-square"></i></a> &nbsp; &nbsp;
+                            <a href="#" type="button" class="btn btn-danger">Move to Trash <i
+                                    class="fa-brands fa-bitbucket"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="d-flex text-body-secondary pt-3">
+                <i class="fa-solid fa-blog" style="position: relative; top:7px; left:26px; color:white"></i>
+                <svg class="bd-placeholder-img flex-shrink-0 me-2 rounded" width="32" height="32"
+                    xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 32x32"
+                    preserveAspectRatio="xMidYMid slice" focusable="false">
+                    <title>Placeholder</title>
+                    <rect width="100%" height="100%" fill="#007bff"></rect><text x="50%" y="50%" fill="#007bff"
+                        dy=".3em">32x32</text>
+                </svg>
+                <div class="pb-3 mb-0 small lh-sm border-bottom w-100">
+                    <div class="d-flex justify-content-between">
+                        <div>
+                            <strong class="text-gray-dark">PostTitle</strong>
+                            <span class="d-block">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci,
+                                in............</span>
+                        </div>
+
+                        <div>
+                            <a href="#" type="button" class="btn btn-warning">Edit <i
+                                    class="fa-solid fa-pen-to-square"></i></a> &nbsp; &nbsp;
+                            <a href="#" type="button" class="btn btn-danger">Move to Trash <i
+                                    class="fa-brands fa-bitbucket"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </x-user-view>
+
 </body>
 
 </html>
